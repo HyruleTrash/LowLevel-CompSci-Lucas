46aa4d21b2f565eadf35e7650297276085a2ef4e
“HyruleTrash” <83176601+HyruleTrash@users.noreply.github.com>
2025-10-09 11:44:06 +0200

    init

--- src/main.cpp
+++ src/main.cpp
@@ -5,40 +5,84 @@
 #include <bitset>
 #include <SFML/Graphics.hpp>
 
-int main() {
-    sf::RenderWindow window;
-    window.create(sf::VideoMode({ 1280, 720 }), "My test window");
-    window.setFramerateLimit(60);
-    window.setVerticalSyncEnabled(true);
+#include <queue>
+#include <thread>
+#include <iostream>
 
-    if (!ImGui::SFML::Init(window))
-        return -1;
+int counter = 0;
+bool done = false;
 
-    sf::Clock deltaClock;
+std::queue<int> goods;
 
-    while (window.isOpen())
-    {
-        // Event Polling
-        while (const std::optional event = window.pollEvent())
-        {
-            ImGui::SFML::ProcessEvent(window, *event);
+void producer() {
+    std::cout << "Starting producer..." << std::endl;
 
-            // "close requested" event: we close the window
-            if (event->is<sf::Event::Closed>())
-                window.close();
-        }
-
-        // Update
-        ImGui::SFML::Update(window, deltaClock.restart());
-        ImGui::ShowDemoWindow();
-
-        // Render
-        window.clear();
-
-        ImGui::SFML::Render(window);
-
-        window.display();
+    for (int i = 0; i < 500; ++i) {
+        goods.push(i);
+        counter++;
     }
 
-	return 0;
+    done = true;
+
+    std::cout << "Finished producer..." << std::endl;
+}
+
+void consumer() {
+    std::cout << "Starting consumer..." << std::endl;
+
+    while (!done) {
+        while (!goods.empty()) {
+            goods.pop();
+            counter--;
+        }
+    }
+
+    std::cout << "Finished consumer..." << std::endl;
+}
+
+int main() {
+    /*
+     sf::RenderWindow window;
+     window.create(sf::VideoMode({ 1280, 720 }), "My test window");
+     window.setFramerateLimit(60);
+     window.setVerticalSyncEnabled(true);
+
+     if (!ImGui::SFML::Init(window))
+         return -1;
+
+     sf::Clock deltaClock;
+
+     while (window.isOpen())
+     {
+         // Event Polling
+         while (const std::optional event = window.pollEvent())
+         {
+             ImGui::SFML::ProcessEvent(window, *event);
+
+             // "close requested" event: we close the window
+             if (event->is<sf::Event::Closed>())
+                 window.close();
+         }
+
+         // Update
+         ImGui::SFML::Update(window, deltaClock.restart());
+         ImGui::ShowDemoWindow();
+
+         // Render
+         window.clear();
+
+         ImGui::SFML::Render(window);
+
+         window.display();
+     }
+    */
+    counter = 0;
+    std::thread producerThread(producer);
+    std::thread consumerThread(consumer);
+
+    producerThread.join();
+    consumerThread.join();
+
+    std::cout << "Net: " << counter << " " << goods.size() << std::endl;
+    return 0;
 }
